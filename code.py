3a9a3fd6792ffdba37cb26ec988b554bbb887e7b
William Buddenberg <1421354+wabudd1@users.noreply.github.com>
2021-09-19 19:24:53 -0400

    Light up all keys with a different color; Stub out keyboard output on all keys;

--- code.py
+++ code.py
@@ -1,10 +1,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2021 Kattni Rembor for Adafruit Industries
 #
 # SPDX-License-Identifier: Unlicense
-"""
-Simpletest demo for MacroPad. Prints the key pressed, the relative position of the rotary
-encoder, and the state of the rotary encoder switch to the serial console.
-"""
 import time
 from rainbowio import colorwheel
 from adafruit_macropad import MacroPad
@@ -13,29 +9,61 @@ macropad = MacroPad()
 
 # Cannot be updated after initialization
 # Uses Simple Text Display library
-text_lines = macropad.display_text(title="MacroPad Info") 
+text_lines = macropad.display_text(title="Swiggity Swooty") 
 # macropad.display_image("blinka.bmp")
-#audio_files = ["macropad_mp3/slow.mp3", "macropad_mp3/happy.mp3", "macropad_mp3/beats.mp3", "macropad_mp3/upbeats.mp3"]
 
-# tones are integers in Hz
-tones = [196, 220, 246, 262, 294, 330, 349, 392, 440, 494, 523, 587]
+# Set default key colors
+for pixel in range(12):
+    macropad.pixels[pixel] = colorwheel((pixel / 12 * 256))
+
+# Main scan loop
 while True:
     key_event = macropad.keys.events.get()
-    if key_event:
-        if key_event.pressed:
+    if key_event: # Some type of key event
+        if key_event.pressed: # a.k.a. Keydown
+            macropad.pixels[key_event.key_number] = (100, 100, 100)
             if key_event.key_number is 0:
+                macropad.keyboard.send(macropad.Keycode.WINDOWS, macropad.Keycode.L)
+            if key_event.key_number is 1:
+                macropad.keyboard.send(macropad.Keycode.CONTROL, macropad.Keycode.C)
+                macropad.keyboard.send(macropad.Keycode.WINDOWS, macropad.Keycode.R)
+                time.sleep(0.5)
+                macropad.keyboard.send(macropad.Keycode.N)
+                macropad.keyboard.send(macropad.Keycode.O)
+                macropad.keyboard.send(macropad.Keycode.T)
+                macropad.keyboard.send(macropad.Keycode.E)
+                macropad.keyboard.send(macropad.Keycode.P)
                 macropad.keyboard.send(macropad.Keycode.A)
-                macropad.pixels[key_event.key_number] = colorwheel(
-                    int(255 / 12) * key_event.key_number
-                )
-            # macropad.start_tone(tones[key_event.key_number])
+                macropad.keyboard.send(macropad.Keycode.D)
+                macropad.keyboard.send(macropad.Keycode.ENTER)
+                time.sleep(0.5)
+                macropad.keyboard.send(macropad.Keycode.CONTROL, macropad.Keycode.V)
+            if key_event.key_number is 2:
+                macropad.keyboard.send(macropad.Keycode.A)
+            if key_event.key_number is 3:
+                macropad.keyboard.send(macropad.Keycode.A)
+            if key_event.key_number is 4:
+                macropad.keyboard.send(macropad.Keycode.A)
+            if key_event.key_number is 5:
+                macropad.keyboard.send(macropad.Keycode.A)
+            if key_event.key_number is 6:
+                macropad.keyboard.send(macropad.Keycode.A)
+            if key_event.key_number is 7:
+                macropad.keyboard.send(macropad.Keycode.A)
+            if key_event.key_number is 8:
+                macropad.keyboard.send(macropad.Keycode.A)
+            if key_event.key_number is 9:
+                macropad.keyboard.send(macropad.Keycode.A)
+            if key_event.key_number is 10:
+                macropad.keyboard.send(macropad.Keycode.A)
+            if key_event.key_number is 11:
+                macropad.keyboard.send(macropad.Keycode.A)
+            if key_event.key_number is 12:
+                macropad.keyboard.send(macropad.Keycode.A)
             text_lines[0].text = f"Key pressed: {key_event.key_number}"
-
-            # if key_event.key_number < len(audio_files):
-            #     macropad.play_file(audio_files[key_event.key_number])
-        else:
-            macropad.pixels.fill((0, 0, 0))
-            # macropad.stop_tone()
+        else: # Keyup
+            macropad.pixels[key_event.key_number] = colorwheel((key_event.key_number / 12 * 256))
+        
     text_lines[1].text = f"Encoder: {macropad.encoder}"
     text_lines[2].text = f"Encoder switch: {macropad.encoder_switch}"
     text_lines.show()
